1a74ecaaa5406cc148918a88f0b83f73115e9590
Sami Carvalho <samigabriele1@gmail.com>
2025-02-18 00:06:50 -0300

    Estruturando e adicionando funcionalidades à tela home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Puxa o email do localStorage
+    const userEmail = localStorage.getItem('email');
+    if (userEmail) {
+      setUser(userEmail); 
+    }
+  }, []);
+
+  const handleLogout = () => {
+    // Remover o email do localStorage
+    localStorage.removeItem('email');
+    
+    // Redirecionar para a página de login
+    window.location.href = '/login';
+  };
+
+  if (!user) {
+    return <p>Você não está logado. Faça login para acessar a página.</p>;
+  }
+
+  return (
+    <section> 
+      <div className='conteudo__home'>
+      <h2 className='conteudo__titulo'>Bem-vindo, {user}</h2>
+      </div>
+      <button onClick={handleLogout} className='botao'><p className='botao__conteudo'>Sair</p></button>
+    </section>
+  );
+};
+
+export default Home;
